4c0fd2ec151df024e4cbf6bee3db1e5896dbe8ed
salauddin kamran <kamransalauddin186@gmail.com>
2026-06-14 20:19:44 +0600

    updated navbar component in this project client

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,53 @@
+import { Link } from "react-router";
+import { HiMiniBars3CenterLeft } from "react-icons/hi2";
+import { IoSearchOutline } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi";
+import { FaRegHeart } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
 const Navbar = () => {
+  const currentUser = true;
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
       <nav className="flex justify-between items-center">
         {/* left side */}
-        <div>Logo & search bar</div>
+        <div className="flex items-center md:gap-16 gap-4">
+          <Link to="/">
+            <HiMiniBars3CenterLeft className="size-6" />
+          </Link>
+          <div className="relative sm:w-72 w-40 space-x-2">
+            <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
+            <input
+              type="text"
+              placeholder="What are you looking for"
+              className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+            />
+          </div>
+        </div>
         {/* right side */}
-        <div>nav items</div>
+        <div className="relative flex items-center md:space-x-3 space-x-2">
+          <div>
+            {currentUser ? (
+              <button>
+                
+              </button>
+            ) : (
+              <Link>
+                <HiOutlineUser className="size-6" />
+              </Link>
+            )}
+          </div>
+
+          <button className="hidden sm:block">
+            <FaRegHeart className="size-6" />
+          </button>
+          <Link
+            to="/cart"
+            className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-md"
+          >
+            <MdOutlineShoppingCart />
+            <span>0</span>
+          </Link>
+        </div>
       </nav>
     </header>
   );
