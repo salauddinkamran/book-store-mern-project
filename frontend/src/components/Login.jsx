2042eec74918dd25a1c99f1fefe8c4f7afbf79f9
salauddin kamran <kamransalauddin186@gmail.com>
2026-06-16 12:42:23 +0600

    update only login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,61 @@
+import { useState } from "react";
+import { Link } from "react-router";
+
 const Login = () => {
+  const [message, setMessage] = useState("");
   return (
     <div className="h-[calc(100vh-120px)] flex justify-center items-center">
       <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h2 className="text-lg font-semibold mb-4">Please Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email Address"
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 text-sm font-bold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow"
+            />
+          </div>
+          {message && (
+            <p className="text-red-500 text-sm italic mb-3">{message}</p>
+          )}
+          <div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none duration-200">
+              Login
+            </button>
+          </div>
+        </form>
+        <p className="align-baseline font-medium mt-4 text-sm">
+          Haven't an account? please{" "}
+          <Link
+            to="/register"
+            className="text-blue-500 underline hover:text-blue-700"
+          >
+            Register
+          </Link>
+        </p>
       </div>
     </div>
   );
