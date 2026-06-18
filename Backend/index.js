251cece222191aa66df17eecc9793b8297b25ab0
salauddin kamran <kamransalauddin186@gmail.com>
2026-06-18 23:18:50 +0600

    update backend file in this project

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
-
+// kamransalauddin186_db_user
+// book_store_mern_app_db_user
+// BDtZZ5k94FpqwBrp
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  await mongoose.connect(process.env.DB_URL);
 
   app.get("/", (req, res) => {
     res.send("Hello World!");
   });
-  main()
-    .then(() => console.log("MongoDB connec successfully!"))
-    .catch((err) => console.log(err));
 }
+
+main()
+  .then(() => console.log("MongoDB connect successfully!"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ Backend/src/books/book.route.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Reouter();
+// post a book
+router.post()
+module.exports = router;
