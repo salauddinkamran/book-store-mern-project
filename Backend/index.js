d1c861bbf96aaf735d2aef025e765d7345dccfb6
salauddin kamran <kamransalauddin186@gmail.com>
2026-06-20 13:20:11 +0600

    update index file or install cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 // kamransalauddin186_db_user
 // book_store_mern_app_db_user
 // BDtZZ5k94FpqwBrp
+
+// middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// routes
+const bookRoutes = require("./src/books/book.route");
+app.use("/api/books", bookRoutes);
+
 async function main() {
   await mongoose.connect(process.env.DB_URL);
-
   app.get("/", (req, res) => {
     res.send("Hello World!");
   });
--- Backend/src/books/book.route.js
+++ Backend/src/books/book.route.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const router = express.Reouter();
+const router = express.Router();
+// post = when submit something fontend to db
+// get = when get something back from db
+// put/patch = when edit or update something
+// delete = when delete something
 // post a book
-router.post()
+router.post("/create-book", async (req, res) => {});
 module.exports = router;
